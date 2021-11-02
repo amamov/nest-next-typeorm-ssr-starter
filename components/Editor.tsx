61a6e52762bf1235fcd046b7a1514963ca2282ca
amamov <amamov@kakao.com>
2021-11-03 01:36:18 +0900

    test

---
+++ components/Editor.tsx
@@ -0,0 +1,15 @@
+import "@toast-ui/editor/dist/toastui-editor.css";
+
+import { Editor } from "@toast-ui/react-editor";
+
+export default function ToastEditor() {
+  return (
+    <Editor
+      initialValue="hello react editor world!"
+      previewStyle="vertical"
+      height="600px"
+      initialEditType="markdown"
+      useCommandShortcut={true}
+    />
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,27 @@
-import { FC } from 'react';
-import type { AppProps /*, AppContext */ } from 'next/app';
-import Sidebar from '../components/sidebar';
+import { FC } from "react";
+import type { AppProps /*, AppContext */ } from "next/app";
+import dynamic from "next/dynamic";
+import Sidebar from "../components/sidebar";
+import Image from "next/image";
+const ToastEditor = dynamic(() => import("../components/Editor"), {
+  ssr: false,
+});
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <div style={{ display: 'flex', maxWidth: 1100 }}>
-      <div style={{ flexBasis: '30%', margin: 25 }}>
-        <Sidebar />
+    <>
+      <title>hello world</title>
+      <div style={{ display: "flex", maxWidth: 1100 }}>
+        <div style={{ flexBasis: "30%", margin: 25 }}>
+          <Sidebar />
+          <ToastEditor />
+          <Image src="/test.png" alt="me" width="64" height="64" />
+        </div>
+        <div style={{ flexBasis: "70%", margin: 25 }}>
+          <Component {...pageProps} />
+        </div>
       </div>
-      <div style={{ flexBasis: '70%', margin: 25 }}>
-        <Component {...pageProps} />
-      </div>
-    </div>
+    </>
   );
 };
 
